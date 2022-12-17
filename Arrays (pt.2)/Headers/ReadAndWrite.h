d9bc9ccb18b084e9f458c3087049b0c136107350
SleepiCaffeine <106104408+SleepiCaffeine@users.noreply.github.com>
2022-12-17 17:09:31 +0200

    Reading functions now can read *n* values
    Added a bool that allows the user to personally specify, if the text file has an initial *n* value.             This commit was made on my phone, in a coffee shop, with no way to validate if this works at all, will test later

--- Arrays (pt.2)/Headers/ReadAndWrite.h
+++ Arrays (pt.2)/Headers/ReadAndWrite.h
@@ -3,27 +3,34 @@
 
 // Text Reading Functions
 // This funcion reads text dynamically, so no need for an initial size 'n' value
-std::vector <float> __f_init__(std::string txt) {
-    float n; std::vector <float> vec;
+std::vector <float> f_read(std::string txt, bool read_n = false) {
+    std::vector <float> vec; float n;
+    std::ifstream IN(“IN/“ + txt “.txt”);
+  
+    while (IN >> n)    // Reads text until the end
+        vec.push_back(n);
 
-    std::ifstream IN("IN/" + txt + ".txt");
-    while (IN >> n)     // Reads text file until the end
-        vec.push_back(n);   
+    if (read_n)
+        vec.pop_front();
     
     IN.close();
     return vec;
 }
 
-std::vector <int> __i_init__(std::string txt) {
-    int n; std::vector <int> vec;
-
-    std::ifstream IN("IN/" + txt + "_in.txt");
+std::vector <int> i_read(std::string txt, bool read_n = false) {
+    std::vector <int> vec; int n;
+    std::ifstream IN(“IN/“ + txt “.txt”);
+  
     while (IN >> n)
-        vec.push_back(n);   
+        vec.push_back(n);
+
+    if (read_n)
+        vec.pop_front();
     
     IN.close();
     return vec;
 }
+
 
 // Printing Functions
 void print_float(std::string txt, std::vector <float> vec) {
