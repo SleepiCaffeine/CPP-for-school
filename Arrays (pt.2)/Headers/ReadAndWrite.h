80aa59672107f7967ef32120cb25e29d4123b440
SleepiCaffeine <106104408+SleepiCaffeine@users.noreply.github.com>
2022-12-20 21:47:27 +0200

    Changed Function names, added text output function
    If there are any errors... ¯\_(ツ)_/¯

--- Arrays (pt.2)/Headers/ReadAndWrite.h
+++ Arrays (pt.2)/Headers/ReadAndWrite.h
@@ -1,50 +1,63 @@
 #include <bits/stdc++.h>
 
 
-// Text Reading Functions
-// This funcion reads text dynamically, so no need for an initial size 'n' value
+// Text Reading Function. Returns <float> Vector
+// Dynamically reads text, if there is an intial 'n' value, set the read_n to true
 std::vector <float> f_read(std::string txt, bool read_n = false) {
     std::vector <float> vec; float n;
-    std::ifstream IN(“IN/“ + txt “.txt”);
+    std::ifstream IN("IN/" + txt + ".txt");
   
-    while (IN >> n)    // Reads text until the end
+    while (IN >> n) 
         vec.push_back(n);
 
     if (read_n)
-        vec.pop_front();
+        vec.erase(vec.begin());
     
     IN.close();
     return vec;
 }
 
+// Text Reading Function. Returns <int> Vector
+// Dynamically reads text if there is an intial 'n' value, set the read_n to true
 std::vector <int> i_read(std::string txt, bool read_n = false) {
     std::vector <int> vec; int n;
-    std::ifstream IN(“IN/“ + txt “.txt”);
+    std::ifstream IN("IN/" + txt + ".txt");
   
     while (IN >> n)
         vec.push_back(n);
 
     if (read_n)
-        vec.pop_front();
+        vec.erase(vec.begin());
     
     IN.close();
     return vec;
 }
 
-
-// Printing Functions
-void print_float(std::string txt, std::vector <float> vec) {
+// Writing function to output <float> vectors
+// Allows for custom outputs as well
+void f_write(std::string txt, std::vector <float> vec) {
     std::ofstream OUT("OUT/" + txt + ".txt");
+
     for (auto& i : vec)    // "Range-based" cycles exist since C++11, so we can use them
         OUT << std::setprecision(2) << i << "\n";
 
     OUT.close();
 }
 
-void print_int(std::string txt, std::vector <int> vec) {
+// Writing function to output <int> vectors
+// Allows for custom outputs as well
+void i_write(std::string txt, std::vector <int> vec) {
     std::ofstream OUT("OUT/" + txt + ".txt");
+
     for (auto& i : vec)
         OUT << std::setprecision(2) << i << "\n";
 
     OUT.close();
-}   
+}
+
+
+void write_text(std::string txt, std::string custom_text) {
+    std::ofstream OUT("OUT/" + txt + ".txt");
+    OUT << custom_text;
+    OUT.close();
+}
