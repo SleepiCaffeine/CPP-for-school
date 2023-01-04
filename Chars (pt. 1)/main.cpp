c0d7dab0eb59b8a554c9a0650ee82904dc2484af
SleepiCaffeine <depressed.machiato@gmail.com>
2023-01-04 17:17:11 +0200

    Test Pull Attempting to create a pull request

---
+++ Chars (pt. 1)/main.cpp
@@ -0,0 +1,8 @@
+// Jesus this is going to be a bit harder than usual, but that's what makes it fun. L E T T H E P A I N A N D C A R N A G E B E G I N !
+#include <iostream>
+#include "Headers/Tasks.h"
+
+int main() {
+    newline_freq("1_in", "1_out");
+    captical_count("2_in", "2_out");
+}
