cb8d3dc245c332ee3b016405590a38c9728b1ae9
SleepiCaffeine <depressed.machiato@gmail.com>
2023-01-05 01:52:49 +0200

    Main file updates

--- Chars (pt. 1)/main.cpp
+++ Chars (pt. 1)/main.cpp
@@ -1,8 +1,13 @@
 // Jesus this is going to be a bit harder than usual, but that's what makes it fun. L E T T H E P A I N A N D C A R N A G E B E G I N !
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <clocale>
 #include "Headers/Tasks.h"
 
 int main() {
     newline_freq("1_in", "1_out");
     captical_count("2_in", "2_out");
-}
+    space_removal("3_in", "3_out"); // first try lets gooooo
+    class_heights("4_in", "4_out");
+} 
